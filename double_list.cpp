77a1099cbbd173d37aaf6a99b052dd035bf352e4
no-glue <nikola_pav@eunet.rs>
2014-10-06 20:28:40 +0200

    try double list

--- double_list.cpp
+++ double_list.cpp
@@ -1,3 +1,14 @@
+#include "node.h"
+#include "double_list.h"
+#include "double_list_walk.h"
+#include <iostream>
+
+using namespace std;
+
 int main() {
+  DoubleList<Node<string>, string> * dlist = new DoubleList<Node<string>, string>();
+  DoubleListWalk<Node<string>, List<Node<string>, string> > * dlwalk = new DoubleListWalk<Node<string>, List<Node<string>, string> >();
+  delete dlwalk;
+  delete dlist;
   return 0;
 }
