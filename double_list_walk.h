9d7b44945910b795ac154497fc4d45e25ca43dd8
no-glue <nikola_pav@eunet.rs>
2014-10-06 20:20:20 +0200

    double list walk

---
+++ double_list_walk.h
@@ -0,0 +1,26 @@
+template<class Node, class List>class DoubleListWalk {
+public:
+  DoubleListWalk():list(NULL) {}
+  DoubleListWalk(List * list):list(list) {}
+  void rewind() {
+    // start from head
+    rewind(position, list);
+  }
+  Node * next() {
+    // get next item
+    return next(position);
+  }
+private:
+  List * list;
+  Node * position;
+  void rewind(Node * & position, List * & list) {
+    // start from head
+    position = list->get_head();
+  }
+  Node * next() {
+    // get next item
+    Node * item = position;
+    if(position) position = position->right;
+    return item;
+  }
+}
