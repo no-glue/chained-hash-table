06be9d48ea77cc9d690bd41b5c6c10edf18f3920
no-glue <nikola_pav@eunet.rs>
2014-10-06 18:11:30 +0200

    next from head

--- double_list.h
+++ double_list.h
@@ -1,6 +1,6 @@
 template<class Node, typename Type>class DoubleList {
 public:
-  DobuleList():head(NULL), tail(NULL) {}
+  DobuleList():head(NULL), tail(NULL), position(NULL) {}
   void add_left(Type key, Type value) {
     // add to the left side of list
     add_left(key, value, head, tail);
@@ -9,9 +9,14 @@ public:
     // add to the right side of list
     add_right(key, value, head, tail);
   }
+  Node * next_from_head() {
+    // get next_from_head item
+    return next_from_head(position, head);
+  }
 private:
   Node * head;
   Node * tail;
+  Node * position;
   void add_left(Type key, Type value, Node * & head, Node * & tail) {
     // add to the left side of the list
     Node * inserted = new Node(key, value);
@@ -33,5 +38,12 @@ private:
     tail->right = inserted;
     inserted->left = tail;
     tail = inserted;
+  }
+  Node * next_from_head(Node * & position, Node * & head) {
+    // get next_from_head item
+    if(!position) return position;
+    Node * item = position;    
+    position = position->right;
+    return item;
   }
 };
