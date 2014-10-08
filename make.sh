ad109cc7b85ed9ac0c61dd940f442c5fa98b97da
no-glue <nikola_pav@eunet.rs>
2014-10-08 23:14:15 +0200

    importer

--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
 g++ -Wall -W -fpermissive -std=c++11 double_list.cpp -o double_list
 g++ -Wall -W -fpermissive -std=c++11 chained_hash_table.cpp -o chained_hash_table
 g++ -Wall -W -fpermissive -std=c++11 chained_hash_table_flag.cpp -o chained_hash_table_flag
+g++ -Wall -W -fpermissive -std=c++11 importer_flag.cpp -o importer_flag
