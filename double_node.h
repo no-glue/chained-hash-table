6061c105ebc19a50165cacf38ccce2baf832118b
no-glue <nikola_pav@eunet.rs>
2014-10-09 14:15:03 +0200

    var len keys

--- double_node.h
+++ double_node.h
@@ -1,10 +1,8 @@
-#define KEY_SIZE 16
-
 template<typename Type>class DoubleNode {
 public:
-  char key[KEY_SIZE];
+  char * key;
   // key for a node
-  char value[KEY_SIZE];
+  char * value;
   // type for a node
   DoubleNode * left;
   // left node
@@ -13,15 +11,19 @@ public:
   DoubleNode():left(NULL), right(NULL) {}
   DoubleNode(Type node_key, Type node_value):left(NULL), right(NULL) {
     int key_len = node_key.length(), value_len = node_value.length(), i = 0;
+    key = new char[key_len];
     for(; i < key_len; i++) key[i] = node_key.at(i);
     key[key_len] = '\0';
+    value = new char[value_len];
     for(i = 0; i < value_len; i++) value[i] = node_value.at(i);
     value[value_len] = '\0';
   }
   DoubleNode(Type node_key, Type node_value, DoubleNode * left, DoubleNode * right):left(left), right(right) {
     int key_len = node_key.length(), value_len = node_value.length(), i = 0;
+    key = new char[key_len];
     for(; i < key_len; i++) key[i] = node_key.at(i);
     key[key_len] = '\0';
+    value = new char[value_len];
     for(i = 0; i < value_len; i++) value[i] = node_value.at(i);
     value[value_len] = '\0';
   }
