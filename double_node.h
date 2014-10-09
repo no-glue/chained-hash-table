4b466ace1b57d115efa0821ba208883c332a6484
no-glue <nikola_pav@eunet.rs>
2014-10-09 14:26:32 +0200

    var key len

--- double_node.h
+++ double_node.h
@@ -12,19 +12,60 @@ public:
   DoubleNode(Type node_key, Type node_value):left(NULL), right(NULL) {
     int key_len = node_key.length(), value_len = node_value.length(), i = 0;
     key = new char[key_len];
-    for(; i < key_len; i++) key[i] = node_key.at(i);
-    key[key_len] = '\0';
     value = new char[value_len];
-    for(i = 0; i < value_len; i++) value[i] = node_value.at(i);
-    value[value_len] = '\0';
+    if(key_len == value_len) {
+      // key len same as value len
+      for(; i < key_len; i++) {
+        key[i] = node_key.at(i);
+        value[i] = node_value.at(i);
+      }
+      key[key_len] = value[value_len] = '\0';
+    } else if(key_len < value_len) {
+      // key len less than value len
+      for(; i < key_len; i++) {
+        key[i] = node_key.at(i);
+        value[i] = node_value.at(i);
+      }
+      key[key_len] = '\0';
+      for(i = key_len; i < value_len; i++) value[i] = node_value.at(i);
+      value[value_len] = '\0';
+    } else {
+      // key len longer than value len
+      for(; i < value_len; i++) {
+        key[i] = node_key.at(i);
+        value[i] = node_value.at(i);
+      }
+      value[value_len] = '\0';
+      for(i = value_len; i < key_len; i++) key[i] = node_key.at(i);
+      key[key_len] = '\0';
+    }
   }
   DoubleNode(Type node_key, Type node_value, DoubleNode * left, DoubleNode * right):left(left), right(right) {
     int key_len = node_key.length(), value_len = node_value.length(), i = 0;
     key = new char[key_len];
-    for(; i < key_len; i++) key[i] = node_key.at(i);
-    key[key_len] = '\0';
     value = new char[value_len];
-    for(i = 0; i < value_len; i++) value[i] = node_value.at(i);
-    value[value_len] = '\0';
+    if(key_len == value_len) {
+      for(; i < key_len; i++) {
+        key[i] = node_key.at(i);
+        value[i] = node_value.at(i);
+      }
+      key[key_len] = value[value_len] = '\0';
+    } else if(key_len < value_len) {
+      for(; i < key_len; i++) {
+        key[i] = node_key.at(i);
+        value[i] = node_value.at(i);
+      }
+      key[key_len] = '\0';
+      for(i = key_len; i < value_len; i++) value[i] = node_value.at(i);
+      value[value_len] = '\0';
+    } else {
+      for(; i < value_len; i++) {
+        key[i] = node_key.at(i);
+        value[i] = node_value.at(i);
+      }
+      value[value_len] = '\0';
+      for(i = value_len; i < key_len; i++) key[i] = node_key.at(i);
+      key[key_len] = '\0';
+    }
   }
 };
