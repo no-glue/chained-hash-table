3c6c436c12d7fbbedb08182e7eca691b5f6ecf09
no-glue <nikola_pav@eunet.rs>
2014-10-14 21:19:00 +0200

    bfs adapter

--- adapter_metrics_table.h
+++ adapter_metrics_table.h
@@ -44,6 +44,47 @@ private:
     return item;
   }
   double breadth_first_search(Table * & table, Walk * & walk) {
+    Node * current;
+    Node * current_running;
+    Node * current_depth;
+    List * running = new List();
+      table->find("startnode", running);
+    List * depth = new List();
+      walk_depth->set_list(depth);
+      // todo walk_depth
+      walk_depth->rewind();
+    List * node = new List();
+      table->find(running->get_head()->value, node);
+      running->make_empty();
+      walk->set_list(node);
+      walk->rewind();
+      while((current = walk->next())) {
+        depth->insert_right("1", "1");
+        table_visited->insert_unique(current->key, current->value);
+        // todo table_visited
+      }
+      walk->rewind();
+    while((current = walk->next())) {
+      current_depth = walk_depth->next();
+      walk_running->unset_list();
+      // todo walk_running
+        running->make_empty();
+        table->find(current->value, running);
+        walk_running->set_list(running);
+        walk_running->rewind();
+        while((current_running = walk_running->next())) {
+          if(table_visited->insert_unique(current_running->key, current_running->value)) {
+            node->insert_right(current_running->key, current_running->value);
+            depth->insert_right(itoa(atoi(current_depth->value) + 1), itoa(atoi(current_depth->value) + 1));
+          }
+        }
+    }
+    walk_running->unset_list();
+    delete running;
+    walk_depth->unset_list();
+    delete depth;
+    walk->unset_list();
+    delete node;
     return 1;
   }
 };
