cefbc6e21d88e13d36d3014cf4d8c9d93baa10ab
no-glue <nikola_pav@eunet.rs>
2014-10-19 02:11:05 +0200

    double node cpy key value

--- double_node.h
+++ double_node.h
@@ -1,3 +1,5 @@
+#include <string.h>
+
 template<typename Type>class DoubleNode {
 public:
   char * key;
@@ -12,60 +14,15 @@ public:
   DoubleNode(Type node_key, Type node_value):left(NULL), right(NULL) {
     int key_len = node_key.length(), value_len = node_value.length(), i = 0;
     key = new char[key_len];
+    strcpy(key, node_key.c_str());
     value = new char[value_len];
-    if(key_len == value_len) {
-      // key len same as value len
-      for(; i < key_len; i++) {
-        key[i] = node_key.at(i);
-        value[i] = node_value.at(i);
-      }
-      key[key_len] = value[value_len] = '\0';
-    } else if(key_len < value_len) {
-      // key len less than value len
-      for(; i < key_len; i++) {
-        key[i] = node_key.at(i);
-        value[i] = node_value.at(i);
-      }
-      key[key_len] = '\0';
-      for(i = key_len; i < value_len; i++) value[i] = node_value.at(i);
-      value[value_len] = '\0';
-    } else {
-      // key len longer than value len
-      for(; i < value_len; i++) {
-        key[i] = node_key.at(i);
-        value[i] = node_value.at(i);
-      }
-      value[value_len] = '\0';
-      for(i = value_len; i < key_len; i++) key[i] = node_key.at(i);
-      key[key_len] = '\0';
-    }
+    strcpy(value, node_value.c_str());
   }
   DoubleNode(Type node_key, Type node_value, DoubleNode * left, DoubleNode * right):left(left), right(right) {
     int key_len = node_key.length(), value_len = node_value.length(), i = 0;
     key = new char[key_len];
+    strcpy(key, node_key.c_str());
     value = new char[value_len];
-    if(key_len == value_len) {
-      for(; i < key_len; i++) {
-        key[i] = node_key.at(i);
-        value[i] = node_value.at(i);
-      }
-      key[key_len] = value[value_len] = '\0';
-    } else if(key_len < value_len) {
-      for(; i < key_len; i++) {
-        key[i] = node_key.at(i);
-        value[i] = node_value.at(i);
-      }
-      key[key_len] = '\0';
-      for(i = key_len; i < value_len; i++) value[i] = node_value.at(i);
-      value[value_len] = '\0';
-    } else {
-      for(; i < value_len; i++) {
-        key[i] = node_key.at(i);
-        value[i] = node_value.at(i);
-      }
-      value[value_len] = '\0';
-      for(i = value_len; i < key_len; i++) key[i] = node_key.at(i);
-      key[key_len] = '\0';
-    }
+    strcpy(value, node_value.c_str());
   }
 };
